48976fcd2dbfdd912bd4981bb54e1ad67b1a2582
ProxPxD <piotr10tutek@poczta.onet.pl>
2024-03-18 09:49:39 +0100

    add notes

--- models/models.py
+++ models/models.py
@@ -7,9 +7,9 @@ class ModelFactory:
     @classmethod
     def build(cls, fuzzy_link: bool, fuzzy_pooling: bool, **layers_kwargs):
         kwargs = {}
-        if fuzzy_link:
+        if fuzzy_link:  # TODO set only on the first as it was showed the best
             kwargs['before_link'] = FuzzyLayer()
             kwargs['after_link'] = DefuzzyLayer()
-        if fuzzy_pooling:
+        if fuzzy_pooling:  # TODO: consider setting only in some places?
             kwargs['pooling'] = FuzzyPooling()
         return UNet(**kwargs)
