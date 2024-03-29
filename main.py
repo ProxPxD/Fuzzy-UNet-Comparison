80480300da053410114f06bf61070431e69365d0
ProxPxD <piotr10tutek@poczta.onet.pl>
2024-03-29 15:09:38 +0100

    rename

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ labels = analysis.load_labels()
 normalize = utils.get_normalize(labels)
 train_loader, val_loader, test_loader = dataset.get_data_generators(normalize)
 writer = SummaryWriter(Params.log_dir)
-to_analyze = ModelFactory.get_models_to_analyze(space)
+to_analyzes = ModelFactory.get_models_to_analyze(space)
 trainer = Trainer(
     writer=writer,
     get_model_path=lambda model_name, epoch, iteration: f'models/{model_name}_e{epoch}',
@@ -24,6 +24,6 @@ trainer = Trainer(
 )
 
 
-for name, model in to_analyze.items():
+for name, model in to_analyzes.items():
     trainer.set_model(model, name)
     trainer.train(train_loader, val_loader)
